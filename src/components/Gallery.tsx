1a24200a550ae2361055034a218fa1809b6da2eb
edberaga <edbertjonnathan@gmail.com>
2025-08-05 02:03:04 +0700

    Edit the gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from 'react';
 import { X } from 'lucide-react';
-import heroSalon from '@/assets/hero-salon.jpg';
-import spaImage from '@/assets/spa-treatment.jpg';
-import antiAgingImage from '@/assets/anti-aging.jpg';
-import massageImage from '@/assets/massage.jpg';
-import productsImage from '@/assets/products.jpg';
-import salonExterior from '@/assets/salon-exterior.jpg';
+import { FaRegCirclePlay } from 'react-icons/fa6';
+import reel1 from '@/assets/issime_beauty_reels_1.jpg';
+import reel2 from '@/assets/issime_beauty_reels_2.png';
+import reel3 from '@/assets/issime_beauty_reels_3.jpg';
+import reel4 from '@/assets/issime_beauty_reels_4.jpg';
+import reel5 from '@/assets/issime_beauty_reels_5.jpg';
+import reel6 from '@/assets/issime_beauty_reels_6.jpg';
+import reel7 from '@/assets/issime_beauty_reels_7.jpg';
+import reel8 from '@/assets/issime_beauty_reels_8.jpg';
+import { Button } from './ui/button';
+
 
 const Gallery = () => {
   const sectionRef = useRef<HTMLElement>(null);
@@ -31,40 +36,66 @@ const Gallery = () => {
 
   const galleryImages = [
     {
-      src: heroSalon,
-      alt: "Luxury beauty salon interior - Issime Beauty Salon Malaysia",
-      title: "Luxury Salon Interior"
+      src: reel1,
+      alt: "Issime Beauty Salon",
+      title: "You are beautiful! Very beauty! Because you have come Issime!",
+      desc: "Our Korean-style hot stone therapy can help you. We are also currently offering a complimentary lymphatic detox session! Leave a message now to book your treatment or contact us.",
+      url: "https://www.facebook.com/reel/1863289187448223"
     },
     {
-      src: spaImage,
-      alt: "Professional spa treatment room - Anti aging treatments",
-      title: "Spa Treatment Room"
+      src: reel2,
+      alt: "Legs Sore Treatment",
+      title: "Legs Sore for Standing all day.",
+      desc: "My legs used to swell and hurt after work, but now they feel so much better! The therapist's technique is extremely professional;",
+      url: "https://www.facebook.com/reel/1907211520092123"
     },
     {
-      src: antiAgingImage,
-      alt: "Anti-aging facial treatment - Youthful Skin with Natural Face Cleansing",
-      title: "Anti-Aging Treatments"
+      src: reel3,
+      alt: "Stone Leg Treatment",
+      title: "Hot Stone Leg Treatment.",
+      desc: "My stubby legs have turned into long legs 😱 Later I discovered it was due to accumulated toxins causing #leg swelling! Fortunately, I went to Issime Beauty to experience their Hot Stone Leg Treatment.",
+      url: "https://www.facebook.com/reel/953074759699525/?s=fb_shorts_profile&stack_idx=0"
     },
     {
-      src: massageImage,
-      alt: "Full body massage therapy - Relaxation and wellness",
-      title: "Massage Therapy"
+      src: reel4,
+      alt: "Head Massage therapy",
+      title: "Korean hot stone therapy!",
+      desc: "If you are facing insomnia, migraines, eye fatigue, or poor complexion 🤫 you definitely need to experience Korean hot stone therapy! It helps eliminate toxins from the head, clear blockages in the head, and instantly liberate your head, making the entire head more relaxed 😘",
+      url: "https://www.facebook.com/reel/387559177424419/?s=fb_shorts_profile&stack_idx=0"
     },
     {
-      src: productsImage,
-      alt: "Natural beauty products - Holistic natural therapy",
-      title: "Beauty Products"
+      src: reel5,
+      alt: "Issimei Beauty Salon",
+      title: "The looks of Issimei Beauty Salon",
+      desc: "When care becomes the norm, beauty also becomes the norm 🫶🏻",
+      url: "https://www.facebook.com/reel/3543028002644998/?s=fb_shorts_profile&stack_idx=0"
     },
     {
-      src: salonExterior,
-      alt: "Issime Beauty Salon exterior - Beauty Salon in Malaysia",
-      title: "Salon Exterior"
-    }
+      src: reel6,
+      alt: "Full Body Message Theraphy",
+      title: "Issime Beauty Message Theraphy",
+      desc: "Don't hold in your anger anymore 😫 This treatment can actually improve bloating and constipation issues, say goodbye to discomfort!",
+      url: "https://www.facebook.com/Issime.beauty/videos/820396712949244/"
+    },
+    {
+      src: reel7,
+      alt: "Anti Aging Treatment",
+      title: "Anti Aging Treatment",
+      desc: "Many women use Korean-style hot stone acupressure to drive away humidity, coldness, and relieve backaches and soreness, and it also has the effect of #anti-aging and #reversingaging",
+      url: "https://www.facebook.com/Issime.beauty/videos/1112662229733338"
+    },
+    {
+      src: reel8,
+      alt: "Full Body Treatment",
+      title: "Full Body Treatment",
+      desc: "#Insomnia and headache❓ Done in 1 hour ✅ #Turned those calf muscles into #longlegs❓ Done in 1 hour ✅ #Hand pain, #numbness, #tightness❓ Done in 1 hour ✅ You heard it right😱 We will show you results in just 1 hour ✅",
+      url: "https://www.facebook.com/Issime.beauty/videos/436154132700374"
+    },
   ];
 
   return (
     <section id="gallery" ref={sectionRef} className="py-20 bg-card">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto lg:px-[8%] md:px-[8%] px-5">
         <div className="text-center mb-16">
           <h1 className='text-s font-bold font-barlow text-primary uppercase mb-3 scroll-animate'>
             Gallery
@@ -78,24 +109,32 @@ const Gallery = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
           {galleryImages.map((image, index) => (
             <div
               key={index}
               className="scroll-animate cursor-pointer group relative overflow-hidden rounded-xl"
               onClick={() => setSelectedImage(index)}
             >
-              <div className="aspect-square relative">
+              <div className="relative">
                 <img
                   src={image.src}
                   alt={image.alt}
                   className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
                 />
-                <div className="absolute inset-0 bg-black/0 group-hover:bg-black/30 transition-colors duration-300" />
+                <div className="absolute inset-0 bg-black/0 group-hover:bg-black/60 transition-colors duration-300" />
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  <div className="text-white text-center">
-                    <h3 className="text-lg font-semibold">{image.title}</h3>
-                    <p className="text-sm">Click to view</p>
+                  <div className="text-white text-center px-5">
+                    <h3 className="py-2 text-lg font-semibold">{image.title}</h3>
+                    <p className="text-sm text-justify">{image.desc}</p>
+                    <Button
+                      variant='outline'
+                      size='icon'
+                      className='w-10 h-10 my-2 rounded-full'
+                      onClick={() => window.open(image.url)}
+                    >
+                      <FaRegCirclePlay className="h-5 w-5" />
+                    </Button>
                   </div>
                 </div>
               </div>
